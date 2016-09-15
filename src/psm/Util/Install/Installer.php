461061596aee14aa85051e00e615a1458b5aef7d
Romain Dunand <romain@1-more-thing.com>
2016-09-15 03:10:47 +0200

    Fix DB structure to add glip to log table type enum

--- src/psm/Util/Install/Installer.php
+++ src/psm/Util/Install/Installer.php
@@ -204,7 +204,7 @@ class Installer {
 			PSM_DB_PREFIX . 'log' => "CREATE TABLE `" . PSM_DB_PREFIX . "log` (
 						  `log_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
 						  `server_id` int(11) unsigned NOT NULL,
-						  `type` enum('status','email','sms','pushover') NOT NULL,
+						  `type` enum('status','email','sms','pushover', 'glip') NOT NULL,
 						  `message` varchar(255) NOT NULL,
 						  `datetime` timestamp NOT NULL default CURRENT_TIMESTAMP,
 						  `user_id` varchar(255) NOT NULL,
